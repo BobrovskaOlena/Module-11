55744ebbfe7722721b5cff10219f73fb315c295d
BobrovskaOlena <lena69114@gmail.com>
2023-03-18 01:34:55 +0200

    5

---
+++ src/Task5/MixedStream.java
@@ -0,0 +1,8 @@
+package Task5;
+
+public class MixedStream {
+    /*Напишіть метод public static <T> Stream<T> zip(Stream<T> first, Stream<T> second)
+    який "перемішує" елементи зі стрімів first та second,
+    зупиняючись тоді, коли у одного зі стрімів закінчаться елементи.*/
+
+}
