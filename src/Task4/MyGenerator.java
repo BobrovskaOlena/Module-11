feaffa72a35b264854557747d90dc68ee7603c1d
BobrovskaOlena <lena69114@gmail.com>
2023-03-18 01:31:55 +0200

    4

--- src/Task4/MyGenerator.java
+++ src/Task4/MyGenerator.java
@@ -1,9 +1,19 @@
 package Task4;
-
+import java.util.stream.Stream;
 public class MyGenerator {
-    private int a;
-    private int c;
-    private int m;
-    public int next(){
-    int i = a*c*m;
-}}
+
+            public static void main(String[] args) {
+            long a = 25214903917L;
+            long c = 11L;
+            long m = (long) Math.pow(2, 48);
+
+            Stream<Long> randomStream = myGenerate(0L, a, c, m);
+            randomStream
+                    .limit(25)
+                    .forEach(System.out::println);
+        }
+
+    public static Stream<Long> myGenerate(long seed, long a, long c, long m) {
+        return Stream.iterate(seed, x -> (a * x + c) % m);
+    }
+    }
